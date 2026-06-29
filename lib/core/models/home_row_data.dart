8ac24d87a4b45077eb589223315e260c54679e0f
anthonyygpz <anthonyygpz@gmail.com>
2026-06-28 21:39:44 -0700

    chore(cleanup): remove obsolete trailer_game, home_row_data, games_notifier and games_state files

--- lib/core/models/home_row_data.dart
+++
@@ -1,9 +0,0 @@
-import 'package:game_tv/features/home/models/game_item.dart';
-
-class HomeRowData {
-  final String category;
-  final List<GameItem> items;
-  final String? targetRoute;
-
-  HomeRowData({required this.category, required this.items, this.targetRoute});
-}
