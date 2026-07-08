6372481fc1406cb0c9575530ae547c2f1a7e8e18
anthonyygpz <anthonyygpz@gmail.com>
2026-07-07 20:52:20 -0700

    chore: rename project from game_tv to gamespotlight and remove media/youtube dependencies

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:game_tv/app.dart';
+import 'package:gamespotlight/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
